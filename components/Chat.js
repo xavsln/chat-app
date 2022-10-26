23956e5eedf8b01392eed1b69dab7f3a92c6b6f3
xavsln <19362582+xavsln@users.noreply.github.com>
2022-10-26 10:46:33 +0200

    Add ActionSheetProvider for access to actions menu

--- components/Chat.js
+++ components/Chat.js
@@ -4,6 +4,7 @@ import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
 
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 // import action button
 import CustomActions from "./CustomActions";
 
@@ -223,32 +224,34 @@ export default class Chat extends React.Component {
     let { screenColor } = this.props.route.params;
 
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: screenColor,
-        }}
-      >
-        <Text>{this.state.loggedInText}</Text>
-        <GiftedChat
-          renderBubble={this.renderBubble.bind(this)}
-          showUserAvatar={true}
-          messages={this.state.messages}
-          renderInputToolbar={this.renderInputToolbar.bind(this)}
-          onSend={(messages) => this.onSend(messages)}
-          user={{
-            _id: this.state.uid,
-            avatar: "https://placeimg.com/140/140/people",
+      <ActionSheetProvider>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: screenColor,
           }}
-          renderActions={this.renderCustomActions.bind(this)}
-          renderCustomView={this.renderCustomView}
-        />
+        >
+          <Text>{this.state.loggedInText}</Text>
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            showUserAvatar={true}
+            messages={this.state.messages}
+            renderInputToolbar={this.renderInputToolbar.bind(this)}
+            onSend={(messages) => this.onSend(messages)}
+            user={{
+              _id: this.state.uid,
+              avatar: "https://placeimg.com/140/140/people",
+            }}
+            renderActions={this.renderCustomActions.bind(this)}
+            renderCustomView={this.renderCustomView}
+          />
 
-        {/* To solve keyboard issues with some Android versions: */}
-        {Platform.OS === "android" ? (
-          <KeyboardAvoidingView behavior='height' />
-        ) : null}
-      </View>
+          {/* To solve keyboard issues with some Android versions: */}
+          {Platform.OS === "android" ? (
+            <KeyboardAvoidingView behavior='height' />
+          ) : null}
+        </View>
+      </ActionSheetProvider>
     );
   }
 }
